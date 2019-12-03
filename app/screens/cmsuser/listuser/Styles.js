5cab1b590190b09fd341a39c7739d4cd0414ef87
DuyDuong <intern.pdduong@cmc.com.vn>
2019-12-03 14:07:32 +0700

    user

---
+++ app/screens/cmsuser/listuser/Styles.js
@@ -0,0 +1,55 @@
+import {StyleSheet} from 'react-native';
+import {CommonStyles} from 'app/assets/styles/CommonStyles';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  item: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    borderBottomWidth:1,
+    borderColor:"gray"
+  },
+  textName: {
+    padding: 8,
+  },
+  title: {
+    fontSize: 32,
+  },
+  iconListUser: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginRight:16
+  },
+  listuser: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#0D0D0D',
+  },
+  desuser: {
+    color: '#606060',
+  },
+  search: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderRadius: 10,
+    borderColor: 'black',
+    borderWidth: 1,
+    margin: 16,
+    height: 36,
+  },
+  more: {
+    flex: 8,
+  },
+  icon: {
+    flex: 0.5,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputSearchUser: {
+    flex: 8,
+  },
+});
+
+export default styles;
